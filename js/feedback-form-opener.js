5aaeac37f3273900f09c3164e71faab1a9ba12ed
George Kazanchev <127313091+GeorgeKazanchev@users.noreply.github.com>
2024-04-28 09:42:21 +0300

    Переименованы константы в скрипте открытия формы обратной связи
    Переименование выполнено с целью устранения возможного конфликта имён.

--- js/feedback-form-opener.js
+++ js/feedback-form-opener.js
@@ -1,16 +1,16 @@
 const writeUsButton = document.querySelector('.contacts .btn');
-const modalWrapper = document.querySelector('.modal-wrapper-feedback');
-const modalWindow = document.querySelector('.modal-feedback');
-const closeModalButton = modalWindow.querySelector('.modal-close');
+const feedbackModalWrapper = document.querySelector('.modal-wrapper-feedback');
+const feedbackModalWindow = document.querySelector('.modal-feedback');
+const closeFeedbackButton = feedbackModalWindow.querySelector('.modal-close');
 
 writeUsButton.addEventListener('click', (evt) => {
     evt.preventDefault();
-    modalWrapper.classList.add('modal-wrapper-show');
-    modalWindow.classList.add('modal-show');
+    feedbackModalWrapper.classList.add('modal-wrapper-show');
+    feedbackModalWindow.classList.add('modal-show');
 });
 
-closeModalButton.addEventListener('click', (evt) => {
+closeFeedbackButton.addEventListener('click', (evt) => {
     evt.preventDefault();
-    modalWrapper.classList.remove('modal-wrapper-show');
-    modalWindow.classList.remove('modal-show');
+    feedbackModalWrapper.classList.remove('modal-wrapper-show');
+    feedbackModalWindow.classList.remove('modal-show');
 });
