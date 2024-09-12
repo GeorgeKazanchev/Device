d5ab9512af1c1b731cb845807e2b9d4bbfc37a52
George Kazanchev <127313091+GeorgeKazanchev@users.noreply.github.com>
2024-09-12 18:50:32 +0300

    Улучшен UX формы обратной связи
    1. Добавлена анимация при открытии формы и её отправке с неполными данными.
    2. Добавлено сохранение вводимых имени пользователя и email в localStorage.
    3. Реализована возможность закрытия формы нажатием клавиши "Esc".

--- js/feedback-form-opener.js
+++ js/feedback-form-opener.js
@@ -1,16 +1,75 @@
-const writeUsButton = document.querySelector('.contacts .btn');
-const feedbackModalWrapper = document.querySelector('.modal-wrapper-feedback');
-const feedbackModalWindow = document.querySelector('.modal-feedback');
-const closeFeedbackButton = feedbackModalWindow.querySelector('.modal-close');
+(function () {
+    const writeUsButton = document.querySelector('.contacts .btn');
+    const modalWrapper = document.querySelector('.modal-wrapper-feedback');
+    const modal = document.querySelector('.modal-feedback');
+    const closeButton = modal.querySelector('.modal-close');
+    const form = modal.querySelector('.feedback__form');
+    const usernameField = modal.querySelector('.feedback__form-field-username input');
+    const emailField = modal.querySelector('.feedback__form-field-email input');
+    const contentField = modal.querySelector('.feedback__form-field-textarea textarea');
 
-writeUsButton.addEventListener('click', (evt) => {
-    evt.preventDefault();
-    feedbackModalWrapper.classList.add('modal-wrapper-show');
-    feedbackModalWindow.classList.add('modal-show');
-});
+    const USERNAME_KEY_IN_STORAGE = 'username';
+    const EMAIL_KEY_IN_STORAGE = 'email';
 
-closeFeedbackButton.addEventListener('click', (evt) => {
-    evt.preventDefault();
-    feedbackModalWrapper.classList.remove('modal-wrapper-show');
-    feedbackModalWindow.classList.remove('modal-show');
-});
+    let isStorageSupported = true;
+    let usernameInStorage = null;
+    let emailInStorage = null;
+
+    try {
+        usernameInStorage = localStorage.getItem(USERNAME_KEY_IN_STORAGE);
+        emailInStorage = localStorage.getItem(EMAIL_KEY_IN_STORAGE);
+    } catch (err) {
+        isStorageSupported = false;
+    }
+
+    writeUsButton.addEventListener('click', (evt) => {
+        evt.preventDefault();
+        modalWrapper.classList.add('modal-wrapper-show');
+        modal.classList.add('modal-show');
+
+        if (usernameInStorage) {
+            usernameField.value = usernameInStorage;
+            contentField.focus();
+        } else {
+            usernameField.focus();
+        }
+
+        if (emailInStorage) {
+            emailField.value = emailInStorage;
+        }
+    });
+
+    form.addEventListener('submit', (evt) => {
+        if (!usernameField.value || !emailField.value || !contentField.value) {
+            evt.preventDefault();
+            modal.classList.remove('modal-error');
+            modal.offsetWidth = modal.offsetWidth;
+            modal.classList.add('modal-error');
+        } else {
+            if (isStorageSupported) {
+                localStorage.setItem(USERNAME_KEY_IN_STORAGE, usernameField.value);
+                localStorage.setItem(EMAIL_KEY_IN_STORAGE, emailField.value);
+            }
+        }
+    })
+
+    const closeModal = () => {
+        modalWrapper.classList.remove('modal-wrapper-show');
+        modal.classList.remove('modal-show');
+        modal.classList.remove('modal-error');
+    };
+
+    closeButton.addEventListener('click', (evt) => {
+        evt.preventDefault();
+        closeModal();
+    });
+
+    window.addEventListener('keydown', (evt) => {
+        if (evt.key === 'Escape') {
+            if (modal.classList.contains('modal-show')) {
+                evt.preventDefault();
+                closeModal();
+            }
+        }
+    });
+})();
--- js/map-modal-opener.js
+++ js/map-modal-opener.js
@@ -1,16 +1,18 @@
-const map = document.querySelector('.contacts__map a');
-const mapModalWrapper = document.querySelector('.modal-wrapper-map');
-const mapModalWindow = document.querySelector('.modal-map');
-const closeMapButton = mapModalWindow.querySelector('.modal-close');
+(function () {
+    const map = document.querySelector('.contacts__map a');
+    const modalWrapper = document.querySelector('.modal-wrapper-map');
+    const modal = document.querySelector('.modal-map');
+    const closeButton = modal.querySelector('.modal-close');
 
-map.addEventListener('click', (evt) => {
-    evt.preventDefault();
-    mapModalWrapper.classList.add('modal-wrapper-show');
-    mapModalWindow.classList.add('modal-show');
-});
+    map.addEventListener('click', (evt) => {
+        evt.preventDefault();
+        modalWrapper.classList.add('modal-wrapper-show');
+        modal.classList.add('modal-show');
+    });
 
-closeMapButton.addEventListener('click', (evt) => {
-    evt.preventDefault();
-    mapModalWrapper.classList.remove('modal-wrapper-show');
-    mapModalWindow.classList.remove('modal-show');
-});
+    closeButton.addEventListener('click', (evt) => {
+        evt.preventDefault();
+        modalWrapper.classList.remove('modal-wrapper-show');
+        modal.classList.remove('modal-show');
+    });
+})();
